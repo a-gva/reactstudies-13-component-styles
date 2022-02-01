0d016c53c0057dfb4498916355917637878d6ae0
Alexy Araujo <96698163+a-gva@users.noreply.github.com>
2022-02-01 12:30:12 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Header text="Hello World" />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
